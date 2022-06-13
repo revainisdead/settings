fdaacf14172bf127f86980970e5e515ba2d2f8c1
Christian Hall <chall@incytestudios.com>
2022-06-13 10:45:40 -0700

    Fix pre-commit script for sk.

--- git_hooks/install-hooks.sh
+++ git_hooks/install-hooks.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# Instructions how to use
+#   1) cd into a to a git repository
+#       ex: `cd ~/summit-knowledge-integration`
+#   2) run this script
+#       ex: `/home/christian/bin/settings/git_hooks/install-hooks.sh`
+
+
 GIT_DIR=$(git rev-parse --git-dir)
 
 echo "Installing hooks . . ."
